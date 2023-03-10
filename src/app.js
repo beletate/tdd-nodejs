4d0706827884a4dd8ed8ea9f3f77ce2c48755bb0
Vinicius Beletate <vinicius_beletate@hotmail.com>
2023-03-10 13:58:05 -0300

    feat(code): organizing the files and created new features to handle connection

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 const app = require('express')();
 const consign = require('consign');
+const knex = require('knex');
+const knexfile = require('../knexfile');
+const knexLogger = require('knex-logger');
 
-consign({ cwd: 'src', verbose: false})
+// TODO criar chaveamento dinâmico
+app.db = knex(knexfile.test);
+
+//app.use(knexLogger(app.db))
+
+consign({ cwd: 'src', verbose: false })
     .include('./config/middlewares.js')
-    .include('./routes')
+    .then('./services')
+    .then('./routes')
     .then('./config/routes.js')
     .into(app)
 
@@ -11,7 +20,13 @@ app.get('/', (req, res) => {
     res.status(200).send();
 })
 
-
-
+// app.db.on('query', (query) => {
+//     console.log({
+//         sql: query.sql,
+//         bindings: query.bindings ? query.bindings.join(',') : ''
+//     })
+// })
+//     .on('query-response', (response) => console.log(response))
+//     .on('error', error => console.log(error))
 
 module.exports = app;
