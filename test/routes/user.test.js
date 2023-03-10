ab1ecfac6558f385678772ff98225728277b6ebe
Vinicius Beletate <vinicius_beletate@hotmail.com>
2023-03-10 13:58:27 -0300

    test(route): testing the user route

--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -1,19 +1,19 @@
 const request = require('supertest');
 
-const app = require('../src/app');
+const app = require('../../src/app');
 
 test('Deve listar todos os usuários', () => {
     return request(app).get('/users')
         .then((res) => {
             expect(res.status).toBe(200);
-            expect(res.body).toHaveLength(1);
-            expect(res.body[0]).toHaveProperty('name', 'Vinicius')
+            expect(res.body.length).toBeGreaterThan(0);
         })
 })
 
 test('Deve inserir usuário com sucesso', () => {
+    const mail = `${Date.now()}@mail.com`
     return request(app).post('/users')
-        .send({name: 'John Dere', mail: 'john@mail.com'})
+        .send({name: 'John Dere', mail, passwd:'123456'})
         .then((res) => {
             expect(res.status).toBe(201);
             expect(res.body.name).toBe('John Dere')
