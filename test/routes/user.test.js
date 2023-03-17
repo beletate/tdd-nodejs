38d7c3a97b9ca2238cc8ac715bdff46ec39c460f
Vinicius Beletate <vinicius_beletate@hotmail.com>
2023-03-17 19:11:05 -0300

    test(refactor): created a new test to check password encrypted

--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -18,7 +18,18 @@ test('Deve inserir usuário com sucesso', () => {
         .then((res) => {
             expect(res.status).toBe(201);
             expect(res.body.name).toBe('John Dere')
+            expect(res.body).not.toHaveProperty('passwd')
         })
+})
+
+test('Deve armanezar senha criptografada', async() => {
+    const res = await request(app).post('/users')
+        .send({name: 'John Dere', mail: `${Date.now()}@mail.com`, passwd:'123456'})
+    expect(res.status).toBe(201);
+    const {id} = res.body;
+    const userDB = await app.services.user.findOne({id})
+    expect(userDB.passwd).not.toBeUndefined();
+    expect(userDB.passwd).not.toBe('123456')
 })
 
 test('Não deve inserir usuário sem nome', () => {
