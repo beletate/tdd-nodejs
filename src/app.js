89a7ac767b3a2b226d990328f16b4e29cfa1fe41
Vinicius Beletate <vinicius_beletate@hotmail.com>
2023-03-17 19:10:31 -0300

    refactor(code): changed error validator and user route

--- src/app.js
+++ src/app.js
@@ -7,20 +7,22 @@ const knexfile = require('../knexfile');
 // TODO criar chaveamento dinâmico
 app.db = knex(knexfile.test);
 
-app.get('/users', (req, res, next) => {
-    res.status(200).send('H4ck3d by 0wn');
-    next()
-})
-
 consign({ cwd: 'src', verbose: false })
     .include('./config/middlewares.js')
     .then('./services')
     .then('./routes')
     .then('./config/routes.js')
-    .into(app)
+    .into(app);
 
 app.get('/', (req, res) => {
-    res.status(200).send();
+    res.status(200).send()
+})
+
+app.use((err, req, res, next) => {
+    const {name, message, stack} = err;
+    if(name === 'ValidationError') res.status(400).json({error: message});
+    else res.status(500).json({name, message, stack})
+    next()
 })
 
 // app.db.on('query', (query) => {
--- src/routes/accounts.js
+++ src/routes/accounts.js
@@ -1,31 +1,35 @@
 module.exports = (app) => {
-    const create = async (req, res) => {
+    const create = async (req, res, next) => {
         try {
             const result = await app.services.account.save(req.body)
             return res.status(201).json(result[0]);
         } catch (err) {
-            return res.status(400).json({ error: err.message })
+            return next(err)
         }
     }
 
-    const findAll = async (req, res) => {
+    const findAll = async (req, res, next) => {
         app.services.account.findAll()
             .then(result => res.status(200).json(result))
+            .catch(err => next(err))
     }
 
-    const findById = async (req, res) => {
+    const findById = async (req, res, next) => {
         app.services.account.findById({ id: req.params.id })
             .then(result => res.status(200).json(result))
+            .catch(err => next(err))
     }
 
-    const update = async (req, res) => {
+    const update = async (req, res, next) => {
         app.services.account.update(req.params.id, req.body)
             .then(result => res.status(200).json(result[0]))
+            .catch(err => next(err))
     }
 
-    const remove = async (req, res) => {
+    const remove = async (req, res, next) => {
         app.services.account.remove(req.params.id)
             .then(() => res.status(204).send())
+            .catch(err => next(err))
     }
 
     return { create, findAll, findById, update, remove }
