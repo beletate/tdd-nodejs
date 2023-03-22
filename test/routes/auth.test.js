a71a320788a9a3487e7c7847d6fb58dc36a3e07e
Vinicius Beletate <vinicius_beletate@hotmail.com>
2023-03-22 10:35:44 -0300

    feat(code, test): route is now safe

--- test/routes/auth.test.js
+++ test/routes/auth.test.js
@@ -1,6 +1,18 @@
 const request = require('supertest');
 const app = require('../../src/app');
 
+test('Deve criar usuário via signup', () => {
+    const mail = `${Date.now()}@mail.com`
+    return request(app).post('/auth/signup')
+        .send({name: 'Walter', mail, passwd:'123456'})
+        .then((res) => {
+            expect(res.status).toBe(201);
+            expect(res.body.name).toBe('Walter');
+            expect(res.body).toHaveProperty('mail');
+            expect(res.body).not.toHaveProperty('passwd')
+        })
+})
+
 test('Deve receber token ao logar', () => {
     const mail = `${Date.now()}@mail.com`
     return app.services.user.save({name: 'Walter', mail, passwd:'123456'})
@@ -29,5 +41,12 @@ test('Não deve autenticar usuário com senha errada', () => {
         .then((res) => {
             expect(res.status).toBe(400);
             expect(res.body.error).toBe('Usuário ou senha inválida.')
+        })
+})
+
+test('Não deve acessar uma rota protegida sem token', () => {
+    return request(app).get('/users')
+        .then((res) => {
+            expect(res.status).toBe(401);
         })
 })
