187150df5cd99535b15d7dcd6009fb077d69a984
Vinicius Beletate <vinicius_beletate@hotmail.com>
2023-03-30 10:55:42 -0300

    feat(code): creating new functions to transaction route

--- src/services/transaction.js
+++ src/services/transaction.js
@@ -7,10 +7,27 @@ module.exports = (app) => {
             .select()
     }
 
+    const findOne = (filter) => {
+        return app.db('transactions')
+            .where(filter)
+            .first()
+    }
+
     const save = (transaction) => {
+        if(transaction.type === 'I' && transaction.ammount < 0 || transaction.type === 'O' && transaction.ammount > 0){
+            transaction.ammount *= -1;
+        }
         return app.db('transactions')
             .insert(transaction, '*');
     }
 
-    return { find, save }
+    const update = (id, transaction) => {
+        return app.db('transactions').where({id}).update(transaction, '*')
+    }
+
+    const remove = (id) => {
+        return app.db('transactions').where({id}).del()
+    }
+
+    return { find, save, findOne, update, remove }
 }
